ae7a457b0e00e6ae5f826267f645d8adafcf2ee0
goku-naoki <naoki.mangoku@di-v.co.jp>
2021-03-30 18:23:11 +0900

    an

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -4,7 +4,24 @@ import "../styles/reset.css";
 import { AppProps } from "next/app";
 import Head from "../components/head";
 import { ThemeProvider } from "styled-components";
+import * as gtag from "../../utils/gtag";
+import React, { useEffect } from "react";
+import { useRouter } from "next/router";
 function MyApp({ Component, pageProps }: AppProps) {
+  const router = useRouter();
+  useEffect(() => {
+    if (!gtag.GA_TRACKING_ID) return;
+
+    const handleRouteChange = (url: string) => {
+      gtag.pageview(url);
+    };
+
+    router.events.on("routeChangeComplete", handleRouteChange);
+    return () => {
+      router.events.off("routeChangeComplete", handleRouteChange);
+    };
+  }, [router.events]);
+
   return (
     <Layout>
       <Component {...pageProps} />
--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,97 +1,5 @@
 import React from "react";
-// import Document, {
-//   Html,
-//   Head,
-//   Main,
-//   NextScript,
-//   DocumentContext,
-//   DocumentInitialProps,
-// } from "next/document";
-// import { ServerStyleSheet } from "styled-components";
-
-// export default class MyDocument extends Document {
-//   // 拡張部ここから〜
-//   static async getInitialProps(
-//     ctx: DocumentContext
-//   ): Promise<DocumentInitialProps> {
-//     const sheet = new ServerStyleSheet();
-//     const originalRenderPage = ctx.renderPage;
-
-//     ctx.renderPage = (): ReturnType<DocumentContext["renderPage"]> =>
-//       originalRenderPage({
-//         enhanceApp: (App) => (props) => sheet.collectStyles(<App {...props} />),
-//       });
-
-//     const initialProps = await Document.getInitialProps(ctx);
-
-//     return {
-//       ...initialProps,
-//       styles: [
-//         ...React.Children.toArray(initialProps.styles),
-//         sheet.getStyleElement(),
-//       ],
-//     };
-//   }
-//   render() {
-//     return (
-//       <Html lang="ja">
-//         <Head>
-//           <meta charSet="utf-8" />
-//           <meta name="viewport" content="width=device-width, initial-scale=1" />
-//           <link
-//             href="https://fonts.googleapis.com/css2?family=Kanit:wght@300&display=swap"
-//             rel="stylesheet"
-//           />
-//         </Head>
-//         <body>
-//           <Main />
-//           <NextScript />
-//         </body>
-//       </Html>
-//     );
-//   }
-//   // 〜拡張部ここまで
-// }
-
-// import Document, { Html, Head, Main, NextScript } from "next/document";
-// import { ServerStyleSheet } from "styled-components";
-// type Props = {
-//   styleTags: any;
-// };
-
-// export default class MyDocument extends Document<Props> {
-//   static getInitialProps({ renderPage }) {
-//     const sheet = new ServerStyleSheet();
-
-//     const page = renderPage((App) => (props) =>
-//       sheet.collectStyles(<App {...props} />)
-//     );
-
-//     const styleTags = sheet.getStyleElement();
-
-//     return { ...page, styleTags };
-//   }
-
-//   render() {
-//     return (
-//       <Html lang="ja">
-//         <Head>
-//           <meta charSet="utf-8" />
-//           {this.props.styleTags}
-//           <meta name="viewport" content="width=device-width, initial-scale=1" />
-//           <link
-//             href="https://fonts.googleapis.com/css2?family=Kanit:wght@300&display=swap"
-//             rel="stylesheet"
-//           />
-//         </Head>
-//         <body>
-//           <Main />
-//           <NextScript />
-//         </body>
-//       </Html>
-//     );
-//   }
-// }
+import { GA_TRACKING_ID } from "../../utils/gtag";
 import Document, {
   Html,
   Head,
@@ -138,6 +46,24 @@ export default class MyDocument extends Document {
             href="https://fonts.googleapis.com/css2?family=Kanit:wght@300&display=swap"
             rel="stylesheet"
           />
+          {GA_TRACKING_ID && (
+            <>
+              <script
+                async
+                src={`https://www.googletagmanager.com/gtag/js?id=${GA_TRACKING_ID}`}
+              ></script>
+              <script
+                dangerouslySetInnerHTML={{
+                  __html: `
+      window.dataLayer = window.dataLayer || [];
+      function gtag(){dataLayer.push(arguments);}
+      gtag('js', new Date());
+
+      gtag('config', '${GA_TRACKING_ID}');`,
+                }}
+              ></script>
+            </>
+          )}
         </Head>
         <body>
           <Main />

f1468a67c6fd3b5d47e9b44ce3af043a2328e1be
gokunaoki <goku_naoki@icloud.com>
2022-01-10 15:51:42 +0900

    fix gtag

--- app/utils/gtag.ts
+++ app/utils/gtag.ts
@@ -1,16 +1,21 @@
 export const GA_TRACKING_ID = process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS_ID;
 
 export const pageview = (url: string) => {
-  window.gtag("config", GA_TRACKING_ID, {
-    page_path: url,
-  });
+  if (typeof window.gtag !== 'undefined') {
+    window.gtag("config", GA_TRACKING_ID, {
+      page_path: url,
+    });
+  }
 };
 
 export const event = ({ action, category, label, value }) => {
   if (!GA_TRACKING_ID) return;
-  window.gtag("event", action, {
-    event_category: category,
-    event_label: JSON.stringify(label),
-    value: value,
-  });
+  if (typeof window.gtag !== 'undefined') {
+    window.gtag("event", action, {
+      event_category: category,
+      event_label: JSON.stringify(label),
+      value: value,
+    });
+  }
+ 
 };
